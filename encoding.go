29346cc6d724656f936ca64f22d916455dd4c09e
ianoshen <scaly.iano@gmail.com>
2013-04-15 14:47:49 +0800

    go fmt

--- encoding.go
+++ encoding.go
@@ -1,11 +1,11 @@
 package gomc
 
 import (
-    "bytes"
-    "encoding/gob"
-    "encoding/json"
-    "errors"
-    "strconv"
+	"bytes"
+	"encoding/gob"
+	"encoding/json"
+	"errors"
+	"strconv"
 )
 
 type EncodingType uint
@@ -13,173 +13,173 @@ type EncodeFunc func(interface{}) ([]byte, error)
 type DecodeFunc func([]byte, interface{}) error
 
 const (
-    _NUMERIC_BASE = 10
+	_NUMERIC_BASE = 10
 )
 
 const (
-    ENCODING_DEFAULT EncodingType = iota
-    ENCODING_GOB
-    ENCODING_JSON
+	ENCODING_DEFAULT EncodingType = iota
+	ENCODING_GOB
+	ENCODING_JSON
 )
 
 var (
-    encoders = map[EncodingType]EncodeFunc{
-        ENCODING_DEFAULT: encodeDefault,
-        ENCODING_GOB: encodeGob,
-        ENCODING_JSON: json.Marshal,
-    }
+	encoders = map[EncodingType]EncodeFunc{
+		ENCODING_DEFAULT: encodeDefault,
+		ENCODING_GOB:     encodeGob,
+		ENCODING_JSON:    json.Marshal,
+	}
 
-    decoders = map[EncodingType]DecodeFunc{
-        ENCODING_DEFAULT: decodeDefault,
-        ENCODING_GOB: decodeGob,
-        ENCODING_JSON: json.Unmarshal,
-    }
+	decoders = map[EncodingType]DecodeFunc{
+		ENCODING_DEFAULT: decodeDefault,
+		ENCODING_GOB:     decodeGob,
+		ENCODING_JSON:    json.Unmarshal,
+	}
 )
 
 func encodingFlag(encoding EncodingType) uint32 {
-    return 1 << encoding
+	return 1 << encoding
 }
 
 func encodeDefault(object interface{}) (buffer []byte, err error) {
-    switch object.(type) {
-    case bool:
-        buffer = strconv.AppendBool(buffer, object.(bool))
-    case int:
-        buffer = strconv.AppendInt(buffer, int64(object.(int)), _NUMERIC_BASE)
-    case int8:
-        buffer = strconv.AppendInt(buffer, int64(object.(int8)), _NUMERIC_BASE)
-    case int16:
-        buffer = strconv.AppendInt(buffer, int64(object.(int16)), _NUMERIC_BASE)
-    case int32:
-        buffer = strconv.AppendInt(buffer, int64(object.(int32)), _NUMERIC_BASE)
-    case int64:
-        buffer = strconv.AppendInt(buffer, object.(int64), _NUMERIC_BASE)
-    case uint:
-        buffer = strconv.AppendUint(buffer, uint64(object.(uint)), _NUMERIC_BASE)
-    case uint8:
-        buffer = strconv.AppendUint(buffer, uint64(object.(uint8)), _NUMERIC_BASE)
-    case uint16:
-        buffer = strconv.AppendUint(buffer, uint64(object.(uint16)), _NUMERIC_BASE)
-    case uint32:
-        buffer = strconv.AppendUint(buffer, uint64(object.(uint32)), _NUMERIC_BASE)
-    case uint64:
-        buffer = strconv.AppendUint(buffer, object.(uint64), _NUMERIC_BASE)
-    case string:
-        buffer = []byte(object.(string))
-    case []byte:
-        buffer = object.([]byte)
-    default:
-        err = errors.New("Invalid object for default encode")
-    }
-    return
+	switch object.(type) {
+	case bool:
+		buffer = strconv.AppendBool(buffer, object.(bool))
+	case int:
+		buffer = strconv.AppendInt(buffer, int64(object.(int)), _NUMERIC_BASE)
+	case int8:
+		buffer = strconv.AppendInt(buffer, int64(object.(int8)), _NUMERIC_BASE)
+	case int16:
+		buffer = strconv.AppendInt(buffer, int64(object.(int16)), _NUMERIC_BASE)
+	case int32:
+		buffer = strconv.AppendInt(buffer, int64(object.(int32)), _NUMERIC_BASE)
+	case int64:
+		buffer = strconv.AppendInt(buffer, object.(int64), _NUMERIC_BASE)
+	case uint:
+		buffer = strconv.AppendUint(buffer, uint64(object.(uint)), _NUMERIC_BASE)
+	case uint8:
+		buffer = strconv.AppendUint(buffer, uint64(object.(uint8)), _NUMERIC_BASE)
+	case uint16:
+		buffer = strconv.AppendUint(buffer, uint64(object.(uint16)), _NUMERIC_BASE)
+	case uint32:
+		buffer = strconv.AppendUint(buffer, uint64(object.(uint32)), _NUMERIC_BASE)
+	case uint64:
+		buffer = strconv.AppendUint(buffer, object.(uint64), _NUMERIC_BASE)
+	case string:
+		buffer = []byte(object.(string))
+	case []byte:
+		buffer = object.([]byte)
+	default:
+		err = errors.New("Invalid object for default encode")
+	}
+	return
 }
 
 func decodeDefault(buffer []byte, object interface{}) (err error) {
-    str := string(buffer)
-    switch object.(type) {
-    case *bool:
-        value := object.(*bool)
-        *value, err = strconv.ParseBool(str)
-    case *int:
-        v, err := strconv.ParseInt(str, _NUMERIC_BASE, 0)
-        if err == nil {
-            value := object.(*int)
-            *value = int(v)
-        }
-    case *int8:
-        v, err := strconv.ParseInt(str, _NUMERIC_BASE, 8)
-        if err == nil {
-            value := object.(*int8)
-            *value = int8(v)
-        }
-    case *int16:
-        v, err := strconv.ParseInt(str, _NUMERIC_BASE, 16)
-        if err == nil {
-            value := object.(*int16)
-            *value = int16(v)
-        }
-    case *int32:
-        v, err := strconv.ParseInt(str, _NUMERIC_BASE, 32)
-        if err == nil {
-            value := object.(*int32)
-            *value = int32(v)
-        }
-    case *int64:
-        value := object.(*int64)
-        *value, err = strconv.ParseInt(str, _NUMERIC_BASE, 64)
-    case *uint:
-        v, err := strconv.ParseUint(str, _NUMERIC_BASE, 0)
-        if err == nil {
-            value := object.(*uint)
-            *value = uint(v)
-        }
-    case *uint8:
-        v, err := strconv.ParseUint(str, _NUMERIC_BASE, 8)
-        if err == nil {
-            value := object.(*uint8)
-            *value = uint8(v)
-        }
-    case *uint16:
-        v, err := strconv.ParseUint(str, _NUMERIC_BASE, 16)
-        if err == nil {
-            value := object.(*uint16)
-            *value = uint16(v)
-        }
-    case *uint32:
-        v, err := strconv.ParseUint(str, _NUMERIC_BASE, 32)
-        if err == nil {
-            value := object.(*uint32)
-            *value = uint32(v)
-        }
-    case *uint64:
-        value := object.(*uint64)
-        *value, err = strconv.ParseUint(str, _NUMERIC_BASE, 64)
-    case *string:
-        value := object.(*string)
-        *value = str
-    case *[]byte:
-        value := object.(*[]byte)
-        *value = buffer
-    default:
-        err = errors.New("Invalid object for default decode")
-    }
-    return
+	str := string(buffer)
+	switch object.(type) {
+	case *bool:
+		value := object.(*bool)
+		*value, err = strconv.ParseBool(str)
+	case *int:
+		v, err := strconv.ParseInt(str, _NUMERIC_BASE, 0)
+		if err == nil {
+			value := object.(*int)
+			*value = int(v)
+		}
+	case *int8:
+		v, err := strconv.ParseInt(str, _NUMERIC_BASE, 8)
+		if err == nil {
+			value := object.(*int8)
+			*value = int8(v)
+		}
+	case *int16:
+		v, err := strconv.ParseInt(str, _NUMERIC_BASE, 16)
+		if err == nil {
+			value := object.(*int16)
+			*value = int16(v)
+		}
+	case *int32:
+		v, err := strconv.ParseInt(str, _NUMERIC_BASE, 32)
+		if err == nil {
+			value := object.(*int32)
+			*value = int32(v)
+		}
+	case *int64:
+		value := object.(*int64)
+		*value, err = strconv.ParseInt(str, _NUMERIC_BASE, 64)
+	case *uint:
+		v, err := strconv.ParseUint(str, _NUMERIC_BASE, 0)
+		if err == nil {
+			value := object.(*uint)
+			*value = uint(v)
+		}
+	case *uint8:
+		v, err := strconv.ParseUint(str, _NUMERIC_BASE, 8)
+		if err == nil {
+			value := object.(*uint8)
+			*value = uint8(v)
+		}
+	case *uint16:
+		v, err := strconv.ParseUint(str, _NUMERIC_BASE, 16)
+		if err == nil {
+			value := object.(*uint16)
+			*value = uint16(v)
+		}
+	case *uint32:
+		v, err := strconv.ParseUint(str, _NUMERIC_BASE, 32)
+		if err == nil {
+			value := object.(*uint32)
+			*value = uint32(v)
+		}
+	case *uint64:
+		value := object.(*uint64)
+		*value, err = strconv.ParseUint(str, _NUMERIC_BASE, 64)
+	case *string:
+		value := object.(*string)
+		*value = str
+	case *[]byte:
+		value := object.(*[]byte)
+		*value = buffer
+	default:
+		err = errors.New("Invalid object for default decode")
+	}
+	return
 }
 
 func encodeGob(object interface{}) (buffer []byte, err error) {
-    buf := new(bytes.Buffer)
-    encoder := gob.NewEncoder(buf)
-    err = encoder.Encode(object)
-    if err != nil {
-        return
-    }
-    buffer = buf.Bytes()
-    return
+	buf := new(bytes.Buffer)
+	encoder := gob.NewEncoder(buf)
+	err = encoder.Encode(object)
+	if err != nil {
+		return
+	}
+	buffer = buf.Bytes()
+	return
 }
 
 func decodeGob(buffer []byte, object interface{}) error {
-    decoder := gob.NewDecoder(bytes.NewBuffer(buffer))
-    return decoder.Decode(object)
+	decoder := gob.NewDecoder(bytes.NewBuffer(buffer))
+	return decoder.Decode(object)
 }
 
 func encode(object interface{}, encoding EncodingType) (buffer []byte, flag uint32, err error) {
-    if buffer, err = encodeDefault(object); err == nil {
-        flag = encodingFlag(ENCODING_DEFAULT)
-    } else if encoder, ok := encoders[encoding]; ok {
-        buffer, err = encoder(object)
-        flag = encodingFlag(encoding)
-    } else {
-        err = errors.New("Unsupported encoding type")
-    }
-    return
+	if buffer, err = encodeDefault(object); err == nil {
+		flag = encodingFlag(ENCODING_DEFAULT)
+	} else if encoder, ok := encoders[encoding]; ok {
+		buffer, err = encoder(object)
+		flag = encodingFlag(encoding)
+	} else {
+		err = errors.New("Unsupported encoding type")
+	}
+	return
 }
 
 func decode(buffer []byte, flag uint32, object interface{}) (err error) {
-    for encoding, decoder := range decoders {
-        if flag & encodingFlag(encoding) != 0 {
-            return decoder(buffer, object)
-        }
-    }
-    err = errors.New("Unsupported decoding flag")
-    return
+	for encoding, decoder := range decoders {
+		if flag&encodingFlag(encoding) != 0 {
+			return decoder(buffer, object)
+		}
+	}
+	err = errors.New("Unsupported decoding flag")
+	return
 }
--- memcached.go
+++ memcached.go
@@ -9,121 +9,120 @@ package gomc
 import "C"
 
 import (
-    "errors"
-    "strings"
-    "time"
+	"errors"
+	"strings"
+	"time"
 )
 
 const (
-    _CONFIG_SERVER_PREFIX = "--SERVER="
-    _CONFIG_SOCKET_PREFIX = "--SOCKET="
+	_CONFIG_SERVER_PREFIX = "--SERVER="
+	_CONFIG_SOCKET_PREFIX = "--SOCKET="
 )
 
 type mcClient struct {
-    mc *C.memcached_st
-    encoding EncodingType
+	mc       *C.memcached_st
+	encoding EncodingType
 }
 
 func NewClient(servers []string) (self *mcClient, err error) {
-    for i, server := range servers {
-        if strings.HasPrefix(server, "/") {
-            servers[i] = _CONFIG_SOCKET_PREFIX + server
-        } else {
-            servers[i] = _CONFIG_SERVER_PREFIX + server
-        }
-    }
-    config := strings.Join(servers, " ")
-    self = new(mcClient)
-    self.mc = C.memcached(C.CString(config), C.size_t(len(config)))
-    if self.mc == nil {
-        err = self.checkError(C.libmemcached_check_configuration(C.CString(config), C.size_t(len(config)), nil, 0))
-        return
-    }
-    return
+	for i, server := range servers {
+		if strings.HasPrefix(server, "/") {
+			servers[i] = _CONFIG_SOCKET_PREFIX + server
+		} else {
+			servers[i] = _CONFIG_SERVER_PREFIX + server
+		}
+	}
+	config := strings.Join(servers, " ")
+	self = new(mcClient)
+	self.mc = C.memcached(C.CString(config), C.size_t(len(config)))
+	if self.mc == nil {
+		err = self.checkError(C.libmemcached_check_configuration(C.CString(config), C.size_t(len(config)), nil, 0))
+		return
+	}
+	return
 }
 
 func (self *mcClient) encode(object interface{}) ([]byte, uint32, error) {
-    return encode(object, self.encoding)
+	return encode(object, self.encoding)
 }
 
 func (self *mcClient) decode(buffer []byte, flag uint32, object interface{}) error {
-    return decode(buffer, flag, object)
+	return decode(buffer, flag, object)
 }
 
 func (self *mcClient) checkError(returnCode C.memcached_return_t) error {
-    if C.memcached_failed(returnCode) {
-        return nil
-    }
-    return errors.New(C.GoString(C.memcached_strerror(self.mc, returnCode)))
+	if C.memcached_failed(returnCode) {
+		return nil
+	}
+	return errors.New(C.GoString(C.memcached_strerror(self.mc, returnCode)))
 }
 
 func (self *mcClient) LastErrorMessage() string {
-    return C.GoString(C.memcached_last_error_message(self.mc))
+	return C.GoString(C.memcached_last_error_message(self.mc))
 }
 
 func (self *mcClient) AddServer(host string, port int, weight uint32) error {
-    return self.checkError(C.memcached_server_add_with_weight(self.mc, C.CString(host), C.in_port_t(port), C.uint32_t(weight)))
+	return self.checkError(C.memcached_server_add_with_weight(self.mc, C.CString(host), C.in_port_t(port), C.uint32_t(weight)))
 }
 
 func (self *mcClient) SetBehavior(behavior C.memcached_behavior_t, value uint64) error {
-    return self.checkError(C.memcached_behavior_set(self.mc, behavior, C.uint64_t(value)))
+	return self.checkError(C.memcached_behavior_set(self.mc, behavior, C.uint64_t(value)))
 }
 
 func (self *mcClient) GetBehavior(behavior C.memcached_behavior_t) uint64 {
-    return uint64(C.memcached_behavior_get(self.mc, behavior))
+	return uint64(C.memcached_behavior_get(self.mc, behavior))
 }
 
 func (self *mcClient) Increment(key string, offset uint32) (value uint64, err error) {
-    err = self.checkError(C.memcached_increment(self.mc, C.CString(key), C.size_t(len(key)), C.uint32_t(offset), (*C.uint64_t)(&value)))
-    return
+	err = self.checkError(C.memcached_increment(self.mc, C.CString(key), C.size_t(len(key)), C.uint32_t(offset), (*C.uint64_t)(&value)))
+	return
 }
 
 func (self *mcClient) Decrement(key string, offset uint32) (value uint64, err error) {
-    err = self.checkError(C.memcached_decrement(self.mc, C.CString(key), C.size_t(len(key)), C.uint32_t(offset), (*C.uint64_t)(&value)))
-    return
+	err = self.checkError(C.memcached_decrement(self.mc, C.CString(key), C.size_t(len(key)), C.uint32_t(offset), (*C.uint64_t)(&value)))
+	return
 }
 
 func (self *mcClient) Delete(key string, expiration time.Duration) error {
-    return self.checkError(C.memcached_delete(self.mc, C.CString(key), C.size_t(len(key)), C.time_t(expiration.Seconds())))
+	return self.checkError(C.memcached_delete(self.mc, C.CString(key), C.size_t(len(key)), C.time_t(expiration.Seconds())))
 }
 
 func (self *mcClient) Exist(key string) error {
-    return self.checkError(C.memcached_exist(self.mc, C.CString(key), C.size_t(len(key))))
+	return self.checkError(C.memcached_exist(self.mc, C.CString(key), C.size_t(len(key))))
 }
 
 func (self *mcClient) FlushBuffers() error {
-    return self.checkError(C.memcached_flush_buffers(self.mc))
+	return self.checkError(C.memcached_flush_buffers(self.mc))
 }
 
 func (self *mcClient) Flush(expiration time.Duration) error {
-    return self.checkError(C.memcached_flush(self.mc, C.time_t(expiration.Seconds())))
+	return self.checkError(C.memcached_flush(self.mc, C.time_t(expiration.Seconds())))
 }
 
 func (self *mcClient) Get(key string, value interface{}) error {
-    return nil
+	return nil
 }
 
 func (self *mcClient) Add(key string, value interface{}, expiration time.Duration) (err error) {
-    buffer, flag, err := self.encode(value)
-    if err != nil {
-        return
-    }
-    return self.checkError(C.memcached_add(self.mc, C.CString(key), C.size_t(len(key)), C.CString(string(buffer)), C.size_t(len(buffer)), C.time_t(expiration.Seconds()), C.uint32_t(flag)))
+	buffer, flag, err := self.encode(value)
+	if err != nil {
+		return
+	}
+	return self.checkError(C.memcached_add(self.mc, C.CString(key), C.size_t(len(key)), C.CString(string(buffer)), C.size_t(len(buffer)), C.time_t(expiration.Seconds()), C.uint32_t(flag)))
 }
 
 func (self *mcClient) Replace(key string, value interface{}, expiration time.Duration) (err error) {
-    buffer, flag, err := self.encode(value)
-    if err != nil {
-        return
-    }
-    return self.checkError(C.memcached_replace(self.mc, C.CString(key), C.size_t(len(key)), C.CString(string(buffer)), C.size_t(len(buffer)), C.time_t(expiration.Seconds()), C.uint32_t(flag)))
+	buffer, flag, err := self.encode(value)
+	if err != nil {
+		return
+	}
+	return self.checkError(C.memcached_replace(self.mc, C.CString(key), C.size_t(len(key)), C.CString(string(buffer)), C.size_t(len(buffer)), C.time_t(expiration.Seconds()), C.uint32_t(flag)))
 }
 
 func (self *mcClient) Set(key string, value interface{}, expiration time.Duration) (err error) {
-    buffer, flag, err := self.encode(value)
-    if err != nil {
-        return
-    }
-    return self.checkError(C.memcached_set(self.mc, C.CString(key), C.size_t(len(key)), C.CString(string(buffer)), C.size_t(len(buffer)), C.time_t(expiration.Seconds()), C.uint32_t(flag)))
+	buffer, flag, err := self.encode(value)
+	if err != nil {
+		return
+	}
+	return self.checkError(C.memcached_set(self.mc, C.CString(key), C.size_t(len(key)), C.CString(string(buffer)), C.size_t(len(buffer)), C.time_t(expiration.Seconds()), C.uint32_t(flag)))
 }
-
